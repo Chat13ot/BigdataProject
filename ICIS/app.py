d7d8a3bf8680541183a229189df19731416dfb39
KeitHLeE94 <wogus4187@naver.com>
2018-08-26 21:09:30 +0900

    github test

--- ICIS/app.py
+++ ICIS/app.py
@@ -1,48 +1,402 @@
 import elasticsearch
-import json
-from flask import Flask, request
-from flask import render_template
+import datetime
+from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+from flask_wtf import FlaskForm, Form
+from wtforms import StringField, PasswordField, DateField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 es_client = elasticsearch.Elasticsearch('localhost:9200')
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/shop-grid')
-def shop_grid():
-    return render_template('shop-grid.html')
+app.secret_key = 'icis secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECURITY_PASSWORD_SALT'] = 'ICIS'
+app.config['SECURITY_PASSWORD_HASH'] = 'sha512_crypt'
+app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
+app.config['WTF_CSRF_SECRET_KEY'] = 'icissecrete'
 
 
-@app.route('/search', methods=['POST'])
-def search():
-    titles = []
-    imgs = []
+db = SQLAlchemy(app)
 
-    search_term = request.form['search']
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(db.Model, UserMixin):
+    seq = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    babyName = db.Column(db.String(255), nullable=False)
+    birthDate = db.Column(db.Date, nullable=False)
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+    # 입력받을 사용자 정보는 여기다 추가하면됨.
+
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+class UserForm(Form):
+    id = StringField('id', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    babyName = StringField('babyName', validators=[DataRequired()])
+    birthDate = DateField('birthDate', validators=[DataRequired()])
+
+
+totalnums = []
+titles = []
+imgs = []
+prices = []
+urls = []
+pageCount = 0
+
+
+def productView(type=None):
+    doc = es_client.search(index=type, body={
+        "query": {
+            "bool": {
+                "must_not": [
+                    {
+                        "match": {
+                            "title": "."
+                        }
+                    }
+                ]
+            }
+        }
+        , "sort": [
+            {
+                "rating": {
+                    "order": "desc"
+                }
+            }
+        ]
+    }, size=240)
+
+    return doc
+
+
+def productSearch(search_term=None):
     doc = es_client.search(index='_all', body={
         "query": {
             "match_phrase": {
                 "title": search_term
             }
         }
-    }, size=999)
+    }, size=240)
 
-    resultCount = len(doc['hits']['hits'])
+    return doc
 
-    # for i in range(resultCount):
-    #     print(json.dumps(doc['hits']['hits'][i]['_source'], ensure_ascii=False, indent=2))
 
-    for i in range(resultCount):
-        titles.append(doc['hits']['hits'][i]['_source']['title'])
+@app.route('/preference', methods=['POST','GET'])
+def insert():
+    userform = UserForm(FlaskForm)
+    if request.method == 'POST':
+        id = request.form['id']
+        password = request.form['password']
+        babyName = request.form['babyName']
+        birthDate = request.form['birthDate'].split('-')
 
-    for i in range(resultCount):
-        imgs.append(doc['hits']['hits'][i]['_source']['img'])
+        print(id, password, babyName, birthDate)
 
-    return render_template('shop-grid.html', titles=titles, imgs=imgs)
+        # User DB에 넣기
+        user = User()
+        user.id = id
+        user.password = password
+        user.babyName = babyName
+        user.birthDate = datetime.date(int(birthDate[0]), int(birthDate[1]), int(birthDate[2]))
+
+        db.session.add(user)
+        db.session.commit()
+
+        return render_template('preference.html')
+
+    # if request.method == 'POST':
+    #     print('post!!!!!!!!!1')
+    #     if userform.validate_on_submit():
+    #         print(userform)
+    #         print('된다!!!')
+    #     else:
+    #         print(userform)
+    #         print(userform.id._value())
+    #         print(userform.password._value())
+    #         print(userform.babyName._value())
+    #         print(userform.birthDate._value())
+
+
+    # if form.validate_on_submit():
+    #     print(form)
+    #     return render_template('index.html')
+
+
+@app.route('/')
+def index():
+    doc_diaper = productView('diaper')
+    totalnums.append(doc_diaper['hits']['total'])
+
+    doc_milkpowder = productView('milkpowder')
+    totalnums.append(doc_milkpowder['hits']['total'])
+
+    doc_snack = productView('snack')
+    totalnums.append(doc_snack['hits']['total'])
+
+    doc_toy = productView('toy')
+    totalnums.append(doc_toy['hits']['total'])
+
+    return render_template('index.html')
+
+
+@app.route('/shop-grid')
+def shop_grid():
+    return render_template('shoplist_diaper.html')
+
+
+@app.route('/search/<page>', methods=['GET', 'POST'])
+def search(page=None):
+    pageIndex = int(page)
+
+    if request.method == 'POST':
+        search_term = request.form['search']
+
+        doc = productSearch(search_term)
+
+        resultCount = len(doc['hits']['hits'])
+
+        global titles
+        global imgs
+        global prices
+        global urls
+        titles = []
+        imgs = []
+        prices = []
+        urls = []
+
+        for item in doc['hits']['hits']:
+            titles.append(item['_source']['title'])
+            imgs.append(item['_source']['img'])
+            prices.append(str(item['_source']['price']) + '원')
+            urls.append(item['_source']['link'])
+
+        global pageCount
+        if resultCount % 12 == 0:
+            pageCount = int(resultCount / 12)
+        else:
+            pageCount = int(resultCount / 12) + 1
+
+        print(pageCount)
+
+        return render_template('searchresult.html',
+                               titles=titles[12 * (pageIndex-1): 12 * pageIndex],
+                               imgs=imgs[12 * (pageIndex-1): 12 * pageIndex],
+                               prices=prices[12 * (pageIndex-1): 12 * pageIndex],
+                               urls=urls[12 * (pageIndex-1): 12 * pageIndex],
+                               currentpage=pageIndex-1,
+                               totalnum=totalnums,
+                               pageCount=pageCount)
+
+    else:
+        return render_template('searchresult.html',
+                               titles=titles[12 * (pageIndex - 1):12 * pageIndex],
+                               imgs=imgs[12 * (pageIndex - 1):12 * pageIndex],
+                               prices=prices[12 * (pageIndex - 1):12 * pageIndex],
+                               urls=urls[12 * (pageIndex - 1):12 * pageIndex],
+                               currentpage=pageIndex - 1,
+                               totalnum=totalnums,
+                               pageCount=pageCount)
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    userform = UserForm(FlaskForm)
+
+    if request.method == 'GET':
+        if userform.validate_on_submit():
+            print('응어차피get안써')
+
+        return render_template('register.html', form=userform)
+
+    else: ## method = 'POST'
+        if userform.validate_on_submit():
+            return render_template('preference.html')
+
+        print(userform.id._value())
+        print(userform.birthDate._value())
+
+        return render_template('register.html', form=userform)
+
+
+@app.route('/prefer')
+def prefer():
+    return render_template('preference.html')
+
+
+@app.route('/products/diaper/<page>')
+def productList_diaper(page=None):
+    pageIndex = int(page)
+
+    global titles
+    global imgs
+    global prices
+    global urls
+
+    titles = []
+    imgs = []
+    prices = []
+    urls = []
+
+    doc_diaper = productView('diaper')
+
+    for item in doc_diaper['hits']['hits']:
+        titles.append(item['_source']['title'])
+        imgs.append(item['_source']['img'])
+        prices.append(str(item['_source']['price']) + '원')
+        urls.append(item['_source']['link'])
+
+    resultCount = len(doc_diaper['hits']['hits'])
+    global pageCount
+    if resultCount % 12 == 0:
+        pageCount = int(resultCount / 12)
+    else:
+        pageCount = int(resultCount / 12) + 1
+
+    return render_template('shoplist_diaper.html',
+                           titles=titles[12 * (pageIndex - 1): 12 * pageIndex],
+                           imgs=imgs[12 * (pageIndex - 1): 12 * pageIndex],
+                           prices=prices[12 * (pageIndex - 1): 12 * pageIndex],
+                           urls=urls[12 * (pageIndex - 1): 12 * pageIndex],
+                           currentpage=pageIndex - 1,
+                           totalnum=totalnums,
+                           pageCount=pageCount)
+
+
+@app.route('/products/milkpowder/<page>')
+def productList_milkpowder(page=None):
+    pageIndex = int(page)
+
+    global titles
+    global imgs
+    global prices
+    global urls
+
+    titles = []
+    imgs = []
+    prices = []
+    urls = []
+
+    doc_milkpowder = productView('milkpowder')
+
+    for item in doc_milkpowder['hits']['hits']:
+        titles.append(item['_source']['title'])
+        imgs.append(item['_source']['img'])
+        prices.append(str(item['_source']['price']) + '원')
+        urls.append(item['_source']['link'])
+
+    resultCount = len(doc_milkpowder['hits']['hits'])
+    global pageCount
+    if resultCount % 12 == 0:
+        pageCount = int(resultCount / 12)
+    else:
+        pageCount = int(resultCount / 12) + 1
+
+    return render_template('shoplist_milkpowder.html',
+                           titles=titles[12 * (pageIndex - 1): 12 * pageIndex],
+                           imgs=imgs[12 * (pageIndex - 1): 12 * pageIndex],
+                           prices=prices[12 * (pageIndex - 1): 12 * pageIndex],
+                           urls=urls[12 * (pageIndex - 1): 12 * pageIndex],
+                           currentpage=pageIndex - 1,
+                           totalnum=totalnums,
+                           pageCount=pageCount)
+
+
+@app.route('/products/snack/<page>')
+def productList_snack(page=None):
+    pageIndex = int(page)
+
+    global titles
+    global imgs
+    global prices
+    global urls
+
+    titles = []
+    imgs = []
+    prices = []
+    urls = []
+
+    doc_snack = productView('snack')
+
+    for item in doc_snack['hits']['hits']:
+        titles.append(item['_source']['title'])
+        imgs.append(item['_source']['img'])
+        prices.append(str(item['_source']['price']) + '원')
+        urls.append(item['_source']['link'])
+
+    resultCount = len(doc_snack['hits']['hits'])
+    global pageCount
+    if resultCount % 12 == 0:
+        pageCount = int(resultCount / 12)
+    else:
+        pageCount = int(resultCount / 12) + 1
+
+    return render_template('shoplist_snack.html',
+                           titles=titles[12 * (pageIndex - 1): 12 * pageIndex],
+                           imgs=imgs[12 * (pageIndex - 1): 12 * pageIndex],
+                           prices=prices[12 * (pageIndex - 1): 12 * pageIndex],
+                           urls=urls[12 * (pageIndex - 1): 12 * pageIndex],
+                           currentpage=pageIndex - 1,
+                           totalnum=totalnums,
+                           pageCount=pageCount)
+
+
+
+@app.route('/products/toy/<page>')
+def productList_toy(page=None):
+    pageIndex = int(page)
+
+    global titles
+    global imgs
+    global prices
+    global urls
+
+    titles = []
+    imgs = []
+    prices = []
+    urls = []
+
+    doc_toy = productView('toy')
+
+    for item in doc_toy['hits']['hits']:
+        titles.append(item['_source']['title'])
+        imgs.append(item['_source']['img'])
+        prices.append(str(item['_source']['price']) + '원')
+        urls.append(item['_source']['link'])
+
+    resultCount = len(doc_toy['hits']['hits'])
+    global pageCount
+    if resultCount % 12 == 0:
+        pageCount = int(resultCount / 12)
+    else:
+        pageCount = int(resultCount / 12) + 1
+
+    return render_template('shoplist_toy.html',
+                           titles=titles[12 * (pageIndex - 1): 12 * pageIndex],
+                           imgs=imgs[12 * (pageIndex - 1): 12 * pageIndex],
+                           prices=prices[12 * (pageIndex - 1): 12 * pageIndex],
+                           urls=urls[12 * (pageIndex - 1): 12 * pageIndex],
+                           currentpage=pageIndex - 1,
+                           totalnum=totalnums,
+                           pageCount=pageCount)
 
 
 if __name__ == '__main__':
